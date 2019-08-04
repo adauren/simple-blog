1d13d202917ecc379c6fdf6aba788a79497a18f2
adauren <frankcawperwood818@gmail.com>
2019-08-04 19:05:06 +0600

    Initial server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.send("Server is up");
+});
+
+app.use("/api/register", require("./routes/register"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/posts", require("./routes/posts"));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
